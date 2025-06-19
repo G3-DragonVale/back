220dde400f2665611f5c1b3d46ba15be6232b309
ladretti <ladrette.irwin@gmail.com>
2025-06-19 10:33:49 +0200

    .env useage

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin:  process.env.FRONT_URL ?? 'http://localhost:3000',
     credentials: true,
   });
 
