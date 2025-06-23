18d6d6ed9d7830ab22484e786dee2246eaa8d4bd
Célian CHAUSSON <celian.chausson@etu.univ-smb.fr>
2025-06-23 10:49:05 +0200

    fix

--- src/encryption/encryption.service.ts
+++ src/encryption/encryption.service.ts
@@ -1,20 +1,27 @@
 // src/encryption/encryption.service.ts
+import Redis from 'ioredis';
 import { Injectable } from '@nestjs/common';
 import * as crypto from 'crypto';
 
 @Injectable()
 export class EncryptionService {
-  private readonly aesSessionKeys = new Map<string, Buffer>();
+  private redis: Redis;
 
-  setAESKey(sessionId: string, key: Buffer) {
-    this.aesSessionKeys.set(sessionId, key);
+  constructor() {
+    this.redis = new Redis({ host: 'redis', port: 6379 });
   }
 
-  getAESKey(sessionId: string): Buffer | undefined {
-    return this.aesSessionKeys.get(sessionId);
+  async setAESKey(sessionId: string, key: Buffer) {
+    await this.redis.set(`aes:${sessionId}`, key.toString('base64'));
+  }
+
+  async getAESKey(sessionId: string): Promise<Buffer | undefined> {
+    const value = await this.redis.get(`aes:${sessionId}`);
+    return value ? Buffer.from(value, 'base64') : undefined;
   }
 
   encryptWithAES(data: string, aesKey: Buffer): string {
+    console.log(data);
     const iv = crypto.randomBytes(16);
     const cipher = crypto.createCipheriv('aes-256-cbc', aesKey, iv);
     let encrypted = cipher.update(data, 'utf8', 'base64');
@@ -22,16 +29,17 @@ export class EncryptionService {
     return JSON.stringify({ iv: iv.toString('base64'), data: encrypted });
   }
 
-  encrypt(data: any, sessionId: string): any {
-    if(!sessionId) {
+  async encrypt(data: any, sessionId: string): Promise<any> {
+    if (!sessionId) {
       throw new Error('Session ID is required for encryption');
     }
-    const aesKey = this.getAESKey(sessionId);
+    const aesKey = await this.getAESKey(sessionId);
     if (!aesKey) {
       throw new Error(`AES key not found for session: ${sessionId}`);
     }
 
-    const jsonData = typeof data === 'object' ? JSON.stringify(data) : String(data);
+    const jsonData =
+      typeof data === 'object' ? JSON.stringify(data) : String(data);
     return this.encryptWithAES(jsonData, aesKey);
   }
 }
